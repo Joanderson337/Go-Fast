ade4132eb6dcd02290355b03af355f6dc560d8d2
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-27 12:55:27 -0300

    feat:sparkles:: upload de arquivos

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { routes } from './routes';
+import path from 'node:path';
 
+mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017')
 
   .then(() => {
     const app = express();
     const port = 3001;
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
     app.use(express.json());
 
