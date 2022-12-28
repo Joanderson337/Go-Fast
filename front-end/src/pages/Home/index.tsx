2cecbe5437377d42148fd0dce47d1cdc64171c49
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-28 14:52:26 -0300

    refactor::recycle: pagina home

---
+++ front-end/src/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Header } from '../../components/Header';
+import { Orders } from '../../components/Orders';
+import { UserContext } from '../../contexts/user.context';
+
+export function Home(){
+  const navigate = useNavigate();
+  const { isAuthenticated } = useContext(UserContext);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login');
+    }
+  }, [isAuthenticated]);
+
+  return(
+    <>
+      <Header />
+      <Orders />
+    </>
+  );
+}
